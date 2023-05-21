5f9921a4b0be34360dc73086458dd131671c7e4e
Nicklas Frahm <nicklas.frahm@gmail.com>
2023-05-21 23:23:30 +0200

    feat(webui): build simple Google OIDC login

--- web/src/routes/login/+page.svelte
+++ web/src/routes/login/+page.svelte
@@ -1,14 +1,53 @@
 <script>
-  function getLoginUrl() {
-    return '#';
-  }
+  import { PUBLIC_GOOGLE_CLIENT_ID } from '$env/static/public';
+  import { onMount } from 'svelte';
+
+  let redirectURI = '';
+  let params = {};
+
+  onMount(() => {
+    redirectURI = `${window.location.origin}/login`;
+    params = parseHash(window.location.hash);
+    if (Object.keys(params).length > 0) {
+      window.location.hash = '';
+    }
+  });
+
+  const parseHash = (hash) => {
+    const data = hash.substring(1);
+
+    if (!data) {
+      return null;
+    }
+
+    return data.split('&').reduce((parsed, pair) => {
+      const params = { ...parsed };
+      const [key, value] = pair.split('=');
+      params[decodeURIComponent(key)] = decodeURIComponent(value);
+      return params;
+    }, {});
+  };
+
+  const GOOGLE_OAUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  const getLoginLink = (redirectURI) => {
+    console.log(redirectURI);
+    const params = new URLSearchParams({
+      client_id: PUBLIC_GOOGLE_CLIENT_ID,
+      redirect_uri: redirectURI,
+      response_type: 'token',
+      scope: 'openid email profile',
+      // prompt: "select_account",
+    });
+    return `${GOOGLE_OAUTH_URL}?${params.toString()}`;
+  };
 </script>
 
 <div class="h-screen w-screen flex justify-center items-center">
-  <div class="p-4 rounded-md border border-slate-800">
-    <a href={getLoginUrl()}>Login with Google</a>
+  <div class="p-4 rounded-md border border-slate-800 overflow-x-clip">
+    <a href={getLoginLink(redirectURI)}>Login with Google</a>
+    <pre>
+      {JSON.stringify(params, null, 2)}
+    </pre>
   </div>
 </div>
-
-<style lang="postcss">
-</style>
