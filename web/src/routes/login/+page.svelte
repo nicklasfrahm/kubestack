b1e1beda616453d74579a57daa11d6b980306ad6
Nicklas Frahm <nicklas.frahm@gmail.com>
2024-01-04 21:52:05 +0100

    feat(web): create prototype for google login

--- web/src/routes/login/+page.svelte
+++ web/src/routes/login/+page.svelte
@@ -1,51 +1,19 @@
 <script>
   import { PUBLIC_GOOGLE_CLIENT_ID } from '$env/static/public';
-  import { onMount } from 'svelte';
+  import GoogleLogin from './GoogleLogin.svelte';
 
-  let redirectURI = '';
   let params = {};
 
-  onMount(() => {
-    redirectURI = `${window.location.origin}/login`;
-    params = parseHash(window.location.hash);
-    if (Object.keys(params).length > 0) {
-      window.location.hash = '';
-    }
-  });
-
-  const parseHash = (hash) => {
-    const data = hash.substring(1);
-
-    if (!data) {
-      return null;
-    }
-
-    return data.split('&').reduce((parsed, pair) => {
-      const params = { ...parsed };
-      const [key, value] = pair.split('=');
-      params[decodeURIComponent(key)] = decodeURIComponent(value);
-      return params;
-    }, {});
-  };
-
-  const GOOGLE_OAUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-  const getLoginLink = (redirectURI) => {
-    console.log(redirectURI);
-    const params = new URLSearchParams({
-      client_id: PUBLIC_GOOGLE_CLIENT_ID,
-      redirect_uri: redirectURI,
-      response_type: 'token',
-      scope: 'openid email profile',
-      // prompt: "select_account",
-    });
-    return `${GOOGLE_OAUTH_URL}?${params.toString()}`;
+  const handleSignInSuccess = (idToken) => {
+    // Handle the ID token as needed.
+    console.log('ID Token:', idToken);
   };
 </script>
 
 <div class="h-screen w-screen flex justify-center items-center">
   <div class="p-4 rounded-md border border-slate-800 overflow-x-clip">
-    <a href={getLoginLink(redirectURI)}>Login with Google</a>
+    <GoogleLogin clientID={PUBLIC_GOOGLE_CLIENT_ID} />
+
     <pre>
       {JSON.stringify(params, null, 2)}
     </pre>
---
+++ web/src/routes/login/GoogleLogin.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { GoogleOAuthProvider } from 'google-oauth-gsi';
+
+  export let clientID;
+
+  const googleProvider = new GoogleOAuthProvider({
+    clientId: clientID,
+    onScriptLoadError: () => console.log('onScriptLoadError'),
+    onScriptLoadSuccess: () => console.log('onScriptLoadSuccess'),
+  });
+
+  const login = googleProvider.useGoogleLogin({
+    flow: 'implicit',
+    clientId: clientID,
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
+</script>
+
+<div
+  class="p-4 rounded-md border border-slate-800 hover:bg-blue-600 hover:text-white cursor-pointer"
+  on:click={login}
+  on:keypress={login}
+>
+  Login with Google
+</div>
